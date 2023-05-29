e6d349c4a952cf6ca99a5b1d32a5d48437f35cc3
aghakaleem <aghakaleem4@gmail.com>
2023-05-29 17:16:45 +0500

    application

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer(props) {
+  return (
+    <footer className='App-footer'>
+      <h3>{props.text}</h3>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='Hero'>
+      <h2>Hero Content</h2>
+      <p>This is the content of the hero section.</p>
+    </div>
+  )
+}
+
+export default Hero
