8092d9cff44db2e8fd0aae740bbcd718c6d4afb1
aghakaleem <aghakaleem4@gmail.com>
2023-05-30 10:19:53 +0500

    task

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <header className='App-header'>
-      <h1>Header</h1>
+      <h1>This is a header</h1>
     </header>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='Hero'>
+      <div className='container'></div>
+      <h1>This is a container</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim
+      </p>
+    </div>
+  )
+}
+
+export default Hero
